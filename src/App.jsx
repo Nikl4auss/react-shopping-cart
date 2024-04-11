8707cd93c792a589b3b53e87c781a92b2de41854
= <=>
2024-04-11 02:18:07 -0300

    Modified the README to track current progress, passed the filters state from App.jsx through Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import { Products } from "./components/products"
-import { products } from "./mocks/products.json"
+import { useState } from "react"
+import { Products } from "./components/Products"
+import { products as initialProducts } from "./mocks/products.json"
+import { Header } from "./components/Header"
 export default function App () {
+  const [products] = useState(initialProducts)
+  const [filters, setFilters] = useState({
+    category: "all",
+    minPrice: 0
+  })
+
+  function filterProducts(products) {
+    return products.filter(product => {
+      return (
+        product.price >= filters.minPrice && 
+        (
+          filters.category === "all" || 
+          product.category === filters.category
+        )
+      )
+    })
+  }
   return (
-    <Products products={products} />
+    <>
+      <Header changeFilters={setFilters}/>
+      <Products products={filterProducts(products)} />
+    </>
   )
 }
