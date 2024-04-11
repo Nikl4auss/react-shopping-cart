4daab172d8183093dcbbbce10cc51f9823f280ec
= <=>
2024-04-10 22:01:10 -0300

    Added the Products componet to the App.jsx file, and updated the README file with the current progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { Products } from "./components/products"
+import { products } from "./mocks/products.json"
 export default function App () {
   return (
-    <h1>Shopping Cart</h1>
+    <Products products={products} />
   )
 }
