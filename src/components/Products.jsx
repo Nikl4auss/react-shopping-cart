c22868a27315fb28333452df49012f2c16b7fa7f
= <=>
2024-04-12 11:01:07 -0300

    Added conditional styles and rendering, depending if the item is in the cart or not

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,28 +1,50 @@
 /* eslint-disable react/prop-types */
-import { AddToCartIcon } from "./icons"
+import { useCart } from "../hooks/useCart"
+import { AddToCartIcon, RemoveFromCartIcon } from "./icons"
 import "./products.css"
 
 export function Products({ products }) {
-    return (
-        <main className="products">
-            <ul>
-                {products.map(product => (
-                    <li key={product.id}>
-                        <img 
-                            src={product.thumbnail}
-                            alt={product.title} 
-                         />
-                         <div>
-                           <p><strong>{product.title}</strong> - <span>${product.price}</span></p> 
-                         </div>
-                         <div>
-                            <button>
-                                <AddToCartIcon />
-                            </button>
-                         </div>
-                    </li>
-                ))}
-            </ul>
-        </main>
-    )
+  const { addToCart, removeFromCart, checkProductIsInCart } = useCart()
+
+  return (
+    <main className="products">
+      <ul>
+        {products.map(product => {
+          const productIsIncart = checkProductIsInCart(product)
+          return (
+            <li key={product.id}>
+              <img
+                src={product.thumbnail}
+                alt={product.title}
+              />
+              <div>
+                <p><strong>{product.title}</strong> - <span>${product.price}</span></p>
+              </div>
+              <div>
+                <button
+                  style={{
+                    backgroundColor:
+                      productIsIncart
+                        ? 'red'
+                        : '#09f'
+                  }}
+                  onClick={() => {
+                    productIsIncart
+                      ? removeFromCart(product)
+                      : addToCart(product)
+                  }}
+                >
+                  {
+                    productIsIncart
+                      ? <RemoveFromCartIcon />
+                      : <AddToCartIcon />
+                  }
+                </button>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </main>
+  )
 }
